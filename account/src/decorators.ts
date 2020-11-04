824919f5f4992b4c54c1f1f525ad13ba858bd648
Sean Wong <wotzhs@gmail.com>
2020-11-05 01:00:56 +0800

    account: add Post decorator

--- account/src/decorators.ts
+++ account/src/decorators.ts
@@ -1,20 +1,35 @@
 import { Route } from "./router";
 // credits: https://nehalist.io/routing-with-typescript-decorators/
 export namespace Methods {
+  const Decorate = (
+    target: object,
+    propertyKey: string,
+    path: string,
+    method: any
+  ): void => {
+    if (!Reflect.hasMetadata("routes", target.constructor)) {
+      Reflect.defineMetadata("routes", [], target.constructor);
+    }
+
+    const routes: Route[] = Reflect.getMetadata("routes", target.constructor);
+
+    routes.push({
+      method,
+      path,
+      methodName: propertyKey,
+    });
+    Reflect.defineMetadata("routes", routes, target.constructor);
+  };
+
   export const Get = (path: string): MethodDecorator => {
     return (target: object, propertyKey: string): void => {
-      if (!Reflect.hasMetadata("routes", target.constructor)) {
-        Reflect.defineMetadata("routes", [], target.constructor);
-      }
+      Decorate(target, propertyKey, path, "get");
+    };
+  };
 
-      const routes: Route[] = Reflect.getMetadata("routes", target.constructor);
-
-      routes.push({
-        method: "get",
-        path,
-        methodName: propertyKey,
-      });
-      Reflect.defineMetadata("routes", routes, target.constructor);
+  export const Post = (path: string): MethodDecorator => {
+    return (target: object, propertyKey: string): void => {
+      Decorate(target, propertyKey, path, "post");
     };
   };
 }
