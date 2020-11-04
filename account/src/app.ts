ec23ff8980387d7304e7cf7a274c4f00235a7522
Sean Wong <wotzhs@gmail.com>
2020-11-05 00:28:57 +0800

    account: serve swagger spec on index path

--- account/src/app.ts
+++ account/src/app.ts
@@ -1,6 +1,9 @@
 import "reflect-metadata";
 import express from "express";
+import { serve, setup } from "swagger-ui-express";
 import Router from "./router";
+import DB from "./db";
+import swaggerSpec from "./swagger.json";
 
 export class App {
   private app: express.Application;
@@ -8,7 +11,13 @@ export class App {
 
   constructor() {
     this.app = express();
+    this.setupSwagger();
     Router.register(this.app);
+    DB.connect();
+  }
+
+  setupSwagger() {
+    this.app.use("/", serve, setup(swaggerSpec));
   }
 
   run() {
