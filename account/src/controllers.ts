c941eb098ed8be183c9e076cb2de3e7988e52edd
Sean Wong <wotzhs@gmail.com>
2020-11-05 00:48:41 +0800

    account: implement create account

--- account/src/controllers.ts
+++ account/src/controllers.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { Controller, Methods } from "./decorators";
 import { AccountService } from "./services";
 
-@Controller("/")
+@Controller("/accounts")
 export class SignUpControlller {
   private accountService: AccountService;
 
@@ -10,8 +10,8 @@ export class SignUpControlller {
     this.accountService = new AccountService();
   }
 
-  @Methods.Get("signup")
-  async signup(req: Request, res: Response) {
+  @Methods.Post("/")
+  async createAccount(req: Request, res: Response) {
     const [resp, err] = await this.accountService.CreateAccount(req);
     if (err) {
       return res.status(400).send(err);
--- account/src/services.ts
+++ account/src/services.ts
@@ -1,9 +1,26 @@
+import { model } from "mongoose";
+import { Schema } from "./models";
 export class AccountService {
+  private memberModel: any;
+  constructor() {
+    this.memberModel = model("accounts", Schema.Account);
+  }
+
   async CreateAccount(req) {
     try {
-      return [{ message: "hello world" }, null];
+      const member = await this.memberModel.create({
+        fullname: req.body.fullname,
+      });
+      return [{ _id: member._id }, null];
     } catch (err) {
-      return [null, err];
+      return [
+        null,
+        {
+          code: 500,
+          type: "Internal",
+          message: err.message,
+        },
+      ];
     }
   }
 }
