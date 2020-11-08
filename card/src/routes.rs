0f7d1914e76b8b2b194d810b004459b606966947
Sean Wong <wotzhs@gmail.com>
2020-11-09 02:36:22 +0800

    card: wip create card

--- card/src/routes.rs
+++ card/src/routes.rs
@@ -1,9 +1,14 @@
+use crate::db;
+use crate::models::CreateCardRequest;
+use rocket_contrib::json::Json;
+
 #[get("/")]
 pub fn list_card() -> &'static str {
     "Hello World"
 }
 
-#[post("/")]
-pub fn create_card() -> &'static str {
+#[post("/", format = "json", data = "<req>")]
+pub fn create_card(conn: db::DbConn, req: Json<CreateCardRequest>) -> &'static str {
+    println!("received: {:?}", req);
     "card created"
 }
