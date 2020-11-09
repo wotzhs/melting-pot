e33dcbe9166cbf589315230ef13277366635794d
Sean Wong <wotzhs@gmail.com>
2020-11-09 22:12:09 +0800

    card: set card fields to pub, set CreateCardRequest fields to pub, and add datetime serializer function

--- card/src/models.rs
+++ card/src/models.rs
@@ -1,19 +1,39 @@
-use rocket::data::{self, FromDataSimple};
-use rocket::http::Status;
-use rocket::{Data, Outcome::*, Request};
-use serde::Deserialize;
-use std::io::Read;
+use chrono::naive::NaiveDateTime;
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
+#[derive(Serialize)]
 pub struct Card {
-    id: Uuid,
-    user_id: String,
-    number: String,
-    created_at: std::time::SystemTime,
-    updated_at: std::time::SystemTime,
+    pub id: Uuid,
+    pub user_id: String,
+    pub number: String,
+
+    #[serde(with = "datetime_serializer")]
+    pub created_at: NaiveDateTime,
+
+    #[serde(with = "datetime_serializer")]
+    pub updated_at: NaiveDateTime,
 }
 
 #[derive(Debug, PartialEq, Eq, Deserialize)]
 pub struct CreateCardRequest {
-    user_id: String,
+    pub user_id: String,
+}
+
+// credit: https://github.com/serde-rs/serde/issues/1151
+pub fn time_to_json(t: NaiveDateTime) -> String {
+    DateTime::<Utc>::from_utc(t, Utc).to_rfc3339()
+}
+
+mod datetime_serializer {
+    use super::*;
+    use serde::Serializer;
+
+    pub fn serialize<S: Serializer>(
+        time: &NaiveDateTime,
+        serializer: S,
+    ) -> Result<S::Ok, S::Error> {
+        time_to_json(time.clone()).serialize(serializer)
+    }
 }
