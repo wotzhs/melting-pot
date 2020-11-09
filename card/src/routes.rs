cb9e086948e1352ee7651812c53e1fb2f5a42d11
Sean Wong <wotzhs@gmail.com>
2020-11-09 22:12:37 +0800

    card: return proper json reponse from create card endpoint

--- card/src/routes.rs
+++ card/src/routes.rs
@@ -1,6 +1,9 @@
 use crate::db;
 use crate::models::CreateCardRequest;
+use crate::services::card;
 use rocket_contrib::json::Json;
+use rocket_contrib::json::JsonValue;
+use std::error::Error;
 
 #[get("/")]
 pub fn list_card() -> &'static str {
@@ -8,7 +11,13 @@ pub fn list_card() -> &'static str {
 }
 
 #[post("/", format = "json", data = "<req>")]
-pub fn create_card(conn: db::DbConn, req: Json<CreateCardRequest>) -> &'static str {
+pub fn create_card(conn: db::DbConn, req: Json<CreateCardRequest>) -> JsonValue {
     println!("received: {:?}", req);
-    "card created"
+    let res = card::create_card(conn, &req.user_id);
+    match res {
+        Ok(card) => json!(card),
+        Err(e) => json!({
+            "message": Error::to_string(&e),
+        }),
+    }
 }
