e0c925bd1967f42cb04c38f41d0e194462602a92
Sean Wong <wotzhs@gmail.com>
2020-11-13 18:52:44 +0800

    promotion: validate promo code in index endpoint

--- promotion/handlers.hpp
+++ promotion/handlers.hpp
@@ -1,14 +1,26 @@
 #ifndef HANDLERS_H
 #define HANDLERS_H
 
-#include <iostream>
 #include <uWebSockets/App.h>
+#include <string_view>
+#include <nlohmann/json.hpp>
+#include "services.hpp"
+
+using json = nlohmann::json;
 
 class Handlers {
 public:
 	static auto HandlePromoCodeValidation() {
-		return [](auto *res, auto req) {
-			res->end("Hello world");
+		return [](auto *res, auto *req) {
+			std::string_view code = req->getQuery("promo");
+
+			if (!code.size()) {
+				res->end(json{{"status", false}}.dump());
+			}
+
+			bool status = Services::ValidatePromoCode(code);
+
+			res->end(json{{"status", status}}.dump());
 		};
 	}
 };
