ebebe9787e2efbf6049bd83c5ce10bb670bf19f1
Sean Wong <wotzhs@gmail.com>
2020-11-16 03:39:06 +0800

    event-store-clients: build event store client as it is no longer generated

--- account/src/clients.ts
+++ account/src/clients.ts
@@ -1,9 +1,14 @@
-import grpc from "grpc";
-import { EventStoreClient } from "../proto/event_store/event_store_grpc_pb";
+import { ChannelCredentials, makeClientConstructor } from "@grpc/grpc-js";
+import * as EventStoreGrpcPb from "../proto/event_store/event_store_grpc_pb";
+
+const EventStoreConsructor = makeClientConstructor(
+  EventStoreGrpcPb["event_store.EventStore"],
+  "EventStoreService"
+);
 
 export default class Clients {
-  static readonly EventStore = new EventStoreClient(
+  static readonly EventStore = new EventStoreConsructor(
     "[::1]:50051",
-    grpc.credentials.createInsecure()
+    ChannelCredentials.createInsecure()
   );
 }
