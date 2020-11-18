7a2c799997058ea6f71d9a83d950775ccecb1887
Sean Wong <wotzhs@gmail.com>
2020-11-18 22:55:58 +0800

    event-store-clients: add basic event store publishing

--- promotion/handlers.hpp
+++ promotion/handlers.hpp
@@ -5,8 +5,10 @@
 #include <string_view>
 #include <nlohmann/json.hpp>
 #include "services.hpp"
+#include "clients.hpp"
 
 using json = nlohmann::json;
+using EventStore = clients::EventStore;
 
 class Handlers {
 public:
@@ -19,10 +21,26 @@ public:
 			}
 
 			bool status = Services::ValidatePromoCode(code);
+			if (status) {
+				EventStore eventPublisher = clients::EventStore("[::1]:50051");
+				event_store::Event event;
+				event.set_name("promotion_applied");
+				event.set_aggregate_id("");
+				event.set_aggregate_type("");
+				event.set_data("");
+				event_store::EventResponse reply;
+				grpc::Status res = eventPublisher.Publish(event, &reply);
+				if (res.ok())  {
+					std::cout << "processed event id: " << reply.event_id() << std::endl;
+				}
+				else {
+					std::cout << res.error_code() << " : " << res.error_message() << std::endl;
+				}
+			}
 
 			res->end(json{{"status", status}}.dump());
 		};
 	}
 };
 
-#endif
+#endif /* HANDLERS_H */
