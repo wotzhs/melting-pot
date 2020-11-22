a34d61a1c2fa382d50b9c71a75d28e128a529ed9
Sean Wong <wotzhs@gmail.com>
2020-11-22 17:43:56 +0800

    nats_c: subscribe to wallet.created

--- promotion/main.cpp
+++ promotion/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include "handlers.hpp"
+#include "workers.hpp"
 
 const int port = 5004;
 
 int main() {
+	natsOptions *nOpts = nullptr;
+	std::unique_ptr<Stan> s = std::unique_ptr<Stan>(new Stan(nOpts, "test", "promotion"));
+	s->Subscribe("durable-promo", "wallet.created");
 	uWS::App()
 		.get("/*", Handlers::HandlePromoCodeValidation())
 		.listen(port, [](auto *token) {
