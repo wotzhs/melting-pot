82c56f3b5aa941559e708afeb8f401d1bbb09afc
Sean Wong <wotzhs@gmail.com>
2020-11-23 00:05:08 +0800

    nats_c: update stan cluster name & client id

--- promotion/main.cpp
+++ promotion/main.cpp
@@ -5,9 +5,18 @@
 const int port = 5004;
 
 int main() {
-	natsOptions *nOpts = nullptr;
-	std::unique_ptr<Stan> s = std::unique_ptr<Stan>(new Stan(nOpts, "test", "promotion"));
-	s->Subscribe("durable-promo", "wallet.created");
+	natsOptions *nOpts       = nullptr;
+	const char  *clusterName = "melting-pot";
+	const char  *clientID    = "promotion";
+
+	std::unique_ptr<Stan> stan = std::unique_ptr<Stan>(new Stan(
+		nOpts, 
+		clusterName, 
+		clientID
+	));
+	
+	stan->Subscribe("durable-promo", "wallet.created");
+
 	uWS::App()
 		.get("/*", Handlers::HandlePromoCodeValidation())
 		.listen(port, [](auto *token) {
