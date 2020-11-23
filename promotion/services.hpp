18f48e2d64f559f56fb2b39c26ccbffd32c6e5a2
Sean Wong <wotzhs@gmail.com>
2020-11-23 22:06:06 +0800

    nats_c: use unordered map to store promo codes and its reward amount

--- promotion/services.hpp
+++ promotion/services.hpp
@@ -1,12 +1,21 @@
 #ifndef SERVICES_H
 #define SERVICES_H
 
+#include <unordered_map>
 #include <string_view>
 
 class Services {
 public:
-	static bool ValidatePromoCode(std::string_view s) {
-		return s == "EARLYBIRD";
+	static std::pair<bool, int> ValidatePromoCode(std::string_view s) {
+		std::unordered_map<std::string_view, int> mp = {
+			{"EARLYBIRD", 25},
+			{"NOTSOEARLYBIRD", 5},
+		};
+
+		bool promoApplied = mp.count(s) > 0;
+		int reward = promoApplied ? mp[s] : 0;
+
+		return std::make_pair(promoApplied, reward);
 	}
 };
 
