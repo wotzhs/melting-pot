77665f2c502fbf585393b6f7bd027291208ece50
Sean Wong <wotzhs@gmail.com>
2020-11-23 22:07:17 +0800

    nats_c: remove stan class from workers, make workers file exclusively to house event handler codes

--- promotion/workers.hpp
+++ promotion/workers.hpp
@@ -1,81 +1,45 @@
 #ifndef WORKERS_H
 #define WORKERS_H
 
-#define NATS_HAS_STREAMING true
-#include <nats/nats.h>
 #include <iostream>
+#include <nlohmann/json.hpp>
+#include <string_view>
+#include "services.hpp"
+#include "clients/clients.hpp"
 
-class Stan {
-	natsStatus s;
-	stanConnOptions    *connOpts   =   nullptr;
-	stanSubOptions     *subOpts    =   nullptr;
-	stanConnection     *sc         =   nullptr;
-	stanSubscription   *sub        =   nullptr;
-	bool               connLost    =   false;
-public:
-	Stan(natsOptions *opts, const char* cluster, const char* clientID) {
-		s = stanConnOptions_Create(&connOpts);
-		if (s == NATS_OK) {
-			s = stanConnOptions_SetNATSOptions(connOpts, opts);
+using json = nlohmann::json;
+using EventStore = clients::EventStore;
+
+namespace workers {
+	void HandleWalletCreated(const char *data) {
+		auto eventData = json::parse(std::string(data));
+		auto code = std::string_view(eventData.value("code", ""));
+		std::pair<bool, int> status = Services::ValidatePromoCode(code);
+		if (status.first) {
+			json promoData = {
+				{"status", status.first},
+				{"reward", status.second},
+			};
+
+			EventStore eventPublisher = EventStore("[::1]:50051");
+
+			event_store::Event event;
+			event.set_name("promotion_applied");
+			event.set_aggregate_id(eventData["aggregate_id"]);
+			event.set_aggregate_type(eventData["aggregate_type"]);
+			event.set_data(promoData.dump());
+			event_store::EventResponse reply;
+
+			grpc::Status res = eventPublisher.Publish(event, &reply);
+
+			if (res.ok()) {
+				std::cout << "processed event id: " << reply.event_id() << std::endl;
+			}
+			else {
+				std::cout << res.error_code() << " : " << res.error_message() << std::endl;
+			}
 		}
+	};
+} // workers
 
-		if (s == NATS_OK) {
-			s = stanConnOptions_SetConnectionLostHandler(connOpts, connectionLostCallBack, &connOpts);
-		}
-
-		if (s == NATS_OK) {
-			s = stanConnection_Connect(&sc, cluster, clientID, connOpts);
-		}
-
-		natsOptions_Destroy(opts);
-		stanConnOptions_Destroy(connOpts);
-	}
-
-	~Stan() {
-		stanSubscription_Close(sub);
-		stanSubscription_Destroy(sub);
-		stanConnection_Destroy(sc);
-		nats_Close();
-	}
-
-	void Subscribe(const char* durableName, const char* subject) {
-		if (s == NATS_OK) {
-			s = stanSubOptions_Create(&subOpts);
-		}
-
-		if (s == NATS_OK) {
-			s = stanSubOptions_SetDurableName(subOpts, durableName);
-		}
-
-		if (s == NATS_OK) {
-			s = stanSubOptions_StartWithLastReceived(subOpts);
-		}
-
-		if (s == NATS_OK) {
-			s = stanConnection_Subscribe(&sub, sc, subject, onMessage, NULL, subOpts);
-		}
-
-		stanSubOptions_Destroy(subOpts);
-	}
-
-private:
-	static void connectionLostCallBack(stanConnection *sc, const char *errText, void *closure) {
-		bool *connLost = (bool*) closure;
-		std::cout << "connection lost: " << errText << std::endl;
-		*connLost = true;
-	}
-
-	static void onMessage(stanConnection *sc, stanSubscription *sub, const char *channel, stanMsg *msg, void *closure) {
-		printf("Received on [%s]: sequence: %" PRIu64 " data: %.*s timestamp: %" PRId64 " redelivered: %s\n",
-			channel,
-			stanMsg_GetSequence(msg),
-			stanMsg_GetDataLength(msg),
-			stanMsg_GetData(msg),
-			stanMsg_GetTimestamp(msg),
-			stanMsg_IsRedelivered(msg) ? "yes" : "no");
-		
-		stanMsg_Destroy(msg);
-	}
-};
-
-#endif
+#endif // WORKERS_H
