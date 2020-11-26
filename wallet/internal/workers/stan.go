c800f9d88e46ee0cf68ef4e035d1ffca9690e51a
Sean Wong <wotzhs@gmail.com>
2020-11-27 00:44:19 +0800

    wallet_stan: implement handle account created event handler

--- wallet/internal/workers/stan.go
+++ wallet/internal/workers/stan.go
@@ -1,7 +1,11 @@
 package workers
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
 	"log"
+	"melting_pot/wallet/internal/services"
 	"proto/event_store"
 
 	stan "github.com/nats-io/stan.go"
@@ -12,5 +16,47 @@ type Stan struct {
 }
 
 func (w *Stan) HandleAccountCreated(m *stan.Msg) {
-	log.Printf("received message %v", m)
+	var eventData struct {
+		UserID string `json:"user_id"`
+	}
+
+	if err := json.Unmarshal(m.Data, &eventData); err != nil {
+		log.Printf("err in unmarshaling eventData: %v", err)
+		return
+	}
+
+	event := event_store.Event{
+		Name:          "wallet_created",
+		AggregateId:   eventData.UserID,
+		AggregateType: "user",
+	}
+
+	wallet, err := services.CreateWallet(eventData.UserID)
+	if err != nil {
+		log.Printf("services.CreateWallet() err: %v", err)
+
+		event.Name = "wallet_create_failed"
+		event.Data = fmt.Sprintf("%v", err)
+		eID, err := w.EvenStoreClient.Publish(context.TODO(), &event)
+		if err != nil {
+			log.Printf("w.EventStoreClient.Publish() err: %v", &event)
+		}
+
+		log.Printf("processed event id: %v", eID)
+
+		return
+	}
+
+	b, err := json.Marshal(wallet)
+	if err != nil {
+		log.Printf("json.Marshal() err: %v", err)
+	}
+
+	event.Data = string(b)
+	eID, err := w.EvenStoreClient.Publish(context.TODO(), &event)
+	if err != nil {
+		log.Printf("w.EventStoreClient.Publish() err: %v", &event)
+	}
+
+	log.Printf("processed event id: %v", eID)
 }
