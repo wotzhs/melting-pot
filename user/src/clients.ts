174b7a57b1f735994ddf8f0575d7e80b9cf528f3
Sean Wong <wotzhs@gmail.com>
2020-11-28 11:11:01 +0800

    user_stan: rework eventstore client class to not be static

--- user/src/clients.ts
+++ user/src/clients.ts
@@ -1,14 +1,34 @@
 import { ChannelCredentials, makeClientConstructor } from "@grpc/grpc-js";
 import * as EventStoreGrpcPb from "../proto/event_store/event_store_grpc_pb";
+import { Event } from "../proto/event_store/event_store_pb";
 
-const EventStoreConsructor = makeClientConstructor(
-  EventStoreGrpcPb["event_store.EventStore"],
-  "EventStoreService"
-);
+export class EventStore {
+  private client;
+  constructor() {
+    const EventStoreConsructor = makeClientConstructor(
+      EventStoreGrpcPb["event_store.EventStore"],
+      "EventStoreService"
+    );
+    this.client = new EventStoreConsructor(
+      "[::1]:50051",
+      ChannelCredentials.createInsecure()
+    );
+  }
 
-export default class Clients {
-  static readonly EventStore = new EventStoreConsructor(
-    "[::1]:50051",
-    ChannelCredentials.createInsecure()
-  );
+  async publish(event: Event): Promise<object> {
+    try {
+      return await new Promise((resolve, reject) => {
+        this.client.publish(event, (err, resp) => {
+          if (err) {
+            return reject(err);
+          }
+          resolve(resp.toObject());
+        });
+      });
+    } catch (e) {
+      console.log("failed to publish event", event);
+      console.log("error", e);
+      throw e;
+    }
+  }
 }
