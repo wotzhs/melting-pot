a9db1c70b882e8c1c2b259c08d7ab37a83dc9fed
Sean Wong <wotzhs@gmail.com>
2020-11-29 01:29:37 +0800

    user_stan: implement promotion applied event handler

--- wallet/internal/workers/stan.go
+++ wallet/internal/workers/stan.go
@@ -40,6 +40,64 @@ func (w *Stan) HandleUserCreated(m *stan.Msg) {
 		event.Data = fmt.Sprintf("%v", err)
 		eID, err := w.EvenStoreClient.Publish(context.TODO(), &event)
 		if err != nil {
+			log.Printf("w.EventStoreClient.Publish() event: %v, err: %v", &event, err)
+		}
+
+		log.Printf("processed event id: %v", eID)
+
+		return
+	}
+
+	pubEventData := struct {
+		WalletID string `json:"wallet_id"`
+		UserID   string `json:"user_id"`
+		Code     string `json:"code"`
+	}{
+		WalletID: wallet.ID.String(),
+		UserID:   wallet.UserID,
+		Code:     eventData.Code,
+	}
+
+	b, err := json.Marshal(pubEventData)
+	if err != nil {
+		log.Printf("json.Marshal() err: %v", err)
+	}
+
+	event.Data = string(b)
+	eID, err := w.EvenStoreClient.Publish(context.TODO(), &event)
+	if err != nil {
+		log.Printf("w.EventStoreClient.Publish() event: %v, err: %v", &event, err)
+	}
+
+	log.Printf("processed event id: %v", eID)
+}
+
+func (w *Stan) HandlePromotionApplied(m *stan.Msg) {
+	var eventData struct {
+		WalletID string  `json:"wallet_id"`
+		Status   bool    `json:"status"`
+		Reward   float32 `json:"reward"`
+	}
+
+	if err := json.Unmarshal(m.Data, &eventData); err != nil {
+		log.Printf("err in unmarshaling eventData: %v", err)
+		return
+	}
+
+	event := event_store.Event{
+		Name:          "wallet_updated",
+		AggregateId:   eventData.WalletID,
+		AggregateType: "user",
+	}
+
+	wallet, err := services.UpdateWallet(eventData.WalletID, eventData.Reward)
+	if err != nil {
+		log.Printf("services.UpdateWallet() err: %v", err)
+
+		event.Name = "wallet_update_failed"
+		event.Data = fmt.Sprintf("%v", err)
+		eID, err := w.EvenStoreClient.Publish(context.TODO(), &event)
+		if err != nil {
 			log.Printf("w.EventStoreClient.Publish() err: %v", &event)
 		}
 
@@ -49,11 +107,11 @@ func (w *Stan) HandleUserCreated(m *stan.Msg) {
 	}
 
 	pubEventData := struct {
-		ID   string `json:"id"`
-		Code string `json:"code"`
+		WalletID string  `json:"wallet_id"`
+		Balance  float32 `json:"balance"`
 	}{
-		ID:   wallet.ID.String(),
-		Code: eventData.Code,
+		WalletID: wallet.ID.String(),
+		Balance:  wallet.Balance,
 	}
 
 	b, err := json.Marshal(pubEventData)
