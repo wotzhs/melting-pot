6d3688142c0717774bce92e180814454242c440e
Sean Wong <wotzhs@gmail.com>
2020-11-28 21:42:21 +0800

    user_stan: add stan client

--- user/src/clients.ts
+++ user/src/clients.ts
@@ -1,6 +1,13 @@
 import { ChannelCredentials, makeClientConstructor } from "@grpc/grpc-js";
 import * as EventStoreGrpcPb from "../proto/event_store/event_store_grpc_pb";
 import { Event } from "../proto/event_store/event_store_pb";
+import {
+  connect,
+  Message,
+  StanOptions,
+  Stan,
+  SubscriptionOptions,
+} from "node-nats-streaming";
 
 export class EventStore {
   private client;
@@ -30,5 +37,32 @@ export class EventStore {
       console.log("error", e);
       throw e;
     }
+  }
+}
+
+export class StanClient {
+  private sc: Stan;
+  constructor() {
+    const clusterName = "melting-pot";
+    const clientId = "user";
+    const opts: StanOptions = {};
+    this.sc = connect(clusterName, clientId, opts);
+    this.sc.on("connect", this.subscribes);
+  }
+
+  subscribes() {
+    const stanWorker = new StanWorker();
+
+    const opts = this.sc.subscriptionOptions();
+    opts.setDurableName("durable-card");
+
+    const walletCreatedSub = this.sc.subscribe("wallet_created", opts);
+    walletCreatedSub.on("message", stanWorker.handleWalletCreated);
+
+    const walletUpdatedSub = this.sc.subscribe("wallet_updated", opts);
+    walletUpdatedSub.on("message", stanWorker.handleWalletUpdated);
+
+    const cardCreatedSub = this.sc.subscribe("card_created", opts);
+    cardCreatedSub.on("message", stanWorker.handleCardCreated);
   }
 }
