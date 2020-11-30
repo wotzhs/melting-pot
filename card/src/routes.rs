2af73fe62dee492885c3bd33b4c5c0860d19e4c5
Sean Wong <wotzhs@gmail.com>
2020-11-30 23:35:29 +0800

    get_user: request-driven: update card list route to accept query filters

--- card/src/routes.rs
+++ card/src/routes.rs
@@ -1,13 +1,14 @@
 use crate::db;
-use crate::models::card::CreateCardRequest;
+use crate::models::card::{CreateCardRequest, Query};
 use crate::services::card;
 use rocket::http::RawStr;
+use rocket::request::LenientForm;
 use rocket_contrib::json::{Json, JsonValue};
 use std::error::Error;
 
-#[get("/")]
-pub fn list_card(conn: db::DbConn) -> JsonValue {
-    let res = card::list_card(conn);
+#[get("/?<query..>")]
+pub fn list_card(conn: db::DbConn, query: Option<LenientForm<Query>>) -> JsonValue {
+    let res = card::list_card(conn, query);
     match res {
         Ok(cards) => json!(cards),
         Err(e) => {
